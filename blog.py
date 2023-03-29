0ef3c7550254cf4a5f23eb6ccb902c847094929b
An4oys1012 <ankos1990@gmail.com>
2023-03-29 15:14:58 +0300

    two commit

--- blog.py
+++ blog.py
@@ -1,23 +1,27 @@
 import sqlite3
+
 from flask import Flask, render_template, request, flash, url_for, redirect
 from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12345'
 
+
 def get_db_connection():
     conn = sqlite3.connect('blog.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+
 def get_post(post_id):
     conn = get_db_connection()
     post = conn.execute("""SELECT * FROM posts WHERE id = ?""",
-                        (post_id, )).fetchone()
+                        (post_id,)).fetchone()
     conn.close()
     if post is None:
         abort(404)
     return post
+
 
 @app.route('/')
 def index():
@@ -26,9 +30,9 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+
 @app.route('/posts/<int:post_id>')
 def post(post_id):
-    #return "THIS IS POST PAGE"
     post = get_post(post_id)
     return render_template('post.html', post=post)
 
@@ -44,15 +48,13 @@ def edit(post_id):
         else:
             conn = get_db_connection()
             conn.execute("""UPDATE posts SET title = ?, content = ? WHERE id = ?""",
-                        (title, content, post_id))
+                         (title, content, post_id))
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
     return render_template('edit.html', post=post)
 
 
-# def create():
-#     return "CREATE POST"
 @app.route('/create-post', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
@@ -63,23 +65,22 @@ def create():
         else:
             conn = get_db_connection()
             conn.execute("""INSERT INTO posts (title, content) VALUES (?,?)""",
-                        (title, content))
+                         (title, content))
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
     return render_template('create.html')
 
-@app.route('/<int:post_id>/delete', methods=['POST',])
+
+@app.route('/<int:post_id>/delete', methods=['POST', ])
 def delete(post_id):
     post = get_post(post_id)
     conn = get_db_connection()
-    conn.execute("""DELETE FROM posts WHERE id = ?""", (post_id, ))
+    conn.execute("""DELETE FROM posts WHERE id = ?""", (post_id,))
     conn.commit()
     conn.close()
     flash(f"Post {post['title']} was successfully eleted")
     return redirect(url_for('index'))
-
-
 
 
 app.run()
